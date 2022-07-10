3302f3aae7ac28620376e3c407c4c1416d78bca8
Benjamin Thorpe <benjaminthorpe19@gmail.com>
2022-07-10 06:57:26 +0000

    Built index page and login form

---
+++ src/lib/components/LoginForm.svelte
@@ -0,0 +1,41 @@
+<script></script>
+
+<form
+	action=""
+	class="rounded-md bg-white p-5 shadow-sm md:p-10"
+	on:submit|preventDefault={() => console.log("logged in")}
+>
+	<fieldset class="grid gap-5">
+		<legend class="font-raleway mb-5 text-lg font-semibold text-gray-500">Login Now</legend>
+
+		<div>
+			<label for="" class="font-raleway block text-sm text-gray-500">Email</label>
+			<input
+				type="email"
+				class="font-raleway block rounded-md border-[1px] p-2 text-gray-600"
+				placeholder="john@gmail.com"
+			/>
+		</div>
+
+		<div>
+			<label for="" class="font-raleway block text-sm text-gray-500">Password</label>
+			<input
+				type="password"
+				class="font-raleway block rounded-md border-[1px] p-2 text-gray-600"
+				placeholder="password"
+			/>
+		</div>
+
+		<button
+			type="submit"
+			class="font-pt-sans block rounded-full bg-gray-700 p-2 font-semibold
+      text-white hover:bg-gray-800"
+		>
+			Login
+		</button>
+	</fieldset>
+
+	<p class="font-pt-sans mt-4 text-center text-sm text-gray-500">
+		Need an Account? <a href="/" class="text-blue-600">Register Here</a>
+	</p>
+</form>
---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<script>
+	import LoginForm from "../lib/components/LoginForm.svelte";
+</script>
+
+<svelte:head>
+	<title>Travail - Login</title>
+</svelte:head>
+
+<main class="bg-slate-200">
+	<section class="grid h-screen lg:grid-cols-2">
+		<div class="grid place-content-center bg-blue-500 p-4 text-center">
+			<h1 class="font-pt-sans z-10 mb-2 text-5xl font-bold text-white">Travail</h1>
+			<p
+				class="font-raleway z-[1] translate-y-[-30%] rotate-3 transform rounded-md bg-slate-700 p-2
+				font-light text-white shadow-lg md:text-xl"
+			>
+				One stop place to get your jobs or hire candidates.
+			</p>
+		</div>
+
+		<div class="grid place-content-center">
+			<LoginForm />
+		</div>
+	</section>
+</main>
