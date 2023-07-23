9b5555927ed98a5add42ca82ebc1fe8f5bc38435
sparrowsl <benthorpe.dev@gmail.com>
2023-07-23 09:29:41 +0000

    feat: display details of logged in user

--- src/routes/(main)/profile/+page.svelte
+++ src/routes/(main)/profile/+page.svelte
@@ -1,1 +1,33 @@
-<h1>Profile</h1>
+<script>
+	import { page } from "$app/stores";
+	import Button from "$lib/components/Button.svelte";
+	import Image from "$lib/components/Image.svelte";
+</script>
+
+<h1 class="text-xl font-semibold text-gray-700">My Profile</h1>
+
+<figure class="flex items-center gap-5 bg-white rounded max-w-2xl py-7 mt-3">
+	<Image src={$page.data.user.avatar} />
+	<figcaption>
+		<h2 class="font-semibold text-gray-700 text-2xl font-ptsans">{$page.data.user.name}</h2>
+		<p class="text-gray-400 text-sm font-semibold mt-2">Real-Estate Agent</p>
+		<p class="flex items-center gap-1 text-gray-700 mt-3">
+			<i class="i-mdi:email-outline text-xl" />
+			{$page.data.user.email}
+		</p>
+		<p class="flex items-center gap-1 text-gray-700 mt-3">
+			<i class="i-mdi:location text-xl" />
+			{$page.data.user?.location ?? "Los Angeles"}
+		</p>
+		<p class="flex items-center gap-1 text-gray-700 mt-3">
+			<i class="i-mdi:phone-outline text-xl" />
+			{$page.data.user?.contact ?? "+232-99-905-600"}
+		</p>
+		<p class="flex items-center gap-1 text-gray-700 mt-3">
+			<i class="i-mdi:city text-xl" />
+			{$page.data.user?.properties?.length ?? 0} properties
+		</p>
+
+		<Button type="button" class="bg-accent px-4 mt-5">Update Account</Button>
+	</figcaption>
+</figure>
--- src/routes/api/v1/users/[id]/+server.js
+++ src/routes/api/v1/users/[id]/+server.js
@@ -1,9 +1,10 @@
+import prisma from "$lib/server/prisma.js";
 import { json } from "@sveltejs/kit";
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ params }) {
-	console.log("getting user with id", params.id);
-	return json({});
+	const user = await prisma.user.findUnique({ where: { id: params.id }, include: { properties: true } });
+	return json({ user });
 }
 
 /** @type {import('./$types').RequestHandler} */
