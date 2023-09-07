88d69bcaadce7f7d0e92eeb1ee107ec99194b60f
sparrowsl <benthorpe.dev@gmail.com>
2023-09-07 15:50:09 +0000

    mode change

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,23 @@
 <script>
+	import { onNavigate } from "$app/navigation";
 	import { browser } from "$app/environment";
 	import { navigating } from "$app/stores";
 	import { fade, scale } from "svelte/transition";
 	import "@unocss/reset/tailwind.css";
 	import "uno.css";
+
+	onNavigate(async (navigation) => {
+		// @ts-ignore
+		if (!document.startViewTransition) return;
+
+		return new Promise((oldStateCaptureResolve) => {
+			// @ts-ignore
+			document.startViewTransition(async () => {
+				oldStateCaptureResolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
 {#if $navigating && browser}
