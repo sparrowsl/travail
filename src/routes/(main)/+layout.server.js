c47fbea9ea71567dd67993898921dc816444e632
sparrowsl <benthorpe.dev@gmail.com>
2024-02-03 10:51:04 +0000

    update biomejs schema

--- src/routes/(main)/+layout.server.js
+++
@@ -1,8 +0,0 @@
-import { redirect } from "@sveltejs/kit";
-
-/** @type {import('./$types').LayoutServerLoad} */
-export async function load({ locals }) {
-	if (!locals.user) redirect(307, "/");
-
-	return { user: locals.user };
-}
--- src/routes/(main)/properties/+page.server.js
+++
@@ -1,8 +0,0 @@
-import db from "$lib/server/db.js";
-
-/** @type {import('./$types').PageServerLoad} */
-export async function load() {
-	const properties = await db.query.propertiesTable.findMany();
-
-	return { properties };
-}
